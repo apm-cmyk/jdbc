962ba660b51d975ccf99b240fd7ba51cd53bbae7
Ana Paula Marinho <anapaulamarinho@id.uff.br>
2021-11-04 10:38:30 -0300

    Inserir dados

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,7 +2,9 @@ package application;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
@@ -23,16 +25,18 @@ public class Program {
 					"INSERT INTO seller"
 					+ "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
 					+ "VALUES"
-					+ "(?,?,?,?,?)");
-			st.setString(1, "Ana Paula Marinho");
-			st.setString(2, "anapaulamarinho@id.uff.br");
+					+ "(?,?,?,?,?)",
+					Statement.RETURN_GENERATED_KEYS);
+			st.setString(1, "XXXX");
+			st.setString(2, "yyyyy@gmail.com");
 			st.setDate(3, new java.sql.Date(sdf.parse("17/12/1991").getTime()));
 			st.setDouble(4, 10000);
 			st.setInt(5, 4);
 			
 			int rowsAffected = st.executeUpdate();
-			System.out.println("Rows Affected:"+rowsAffected);
-			
+			ResultSet rs = st.getGeneratedKeys();
+			int id = rs.getInt(1);
+			System.out.println("Rows Affected:"+rowsAffected+" Keys:"+rs);
 		}
 		catch(SQLException e) {
 			e.printStackTrace();
